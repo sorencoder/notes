a346014daaa96af4954570f1b38189b5d2431ab9
sorencoder <69846992+sorencoder@users.noreply.github.com>
2022-08-21 08:45:36 +0530

    Update notes.js

--- backend/src/routes/notes.js
+++ backend/src/routes/notes.js
@@ -2,10 +2,14 @@ const express = require('express');
 const router = express.Router();
 const Note = require('./../model/notes');
 
+
+//fetch all the notes belonging to a particular user
 router.post("/lists",async function(req, res){
     var notes = await Note.find({userid: req.body.userid});
     res.json(notes);
 });
+
+//add or update a note
 router.post("/add",async function(req, res){ 
     await Note.deleteOne({id:req.body.id});
     var newNotes = new Note({
@@ -18,6 +22,8 @@ router.post("/add",async function(req, res){
     const response = {message:"new note created "+ `id: ${req.body.id}`};
     res.json(response);
 });
+
+//delete a note
 router.post("/delete" , async function(req, res){
     await Note.deleteOne({id: req.body.id});
     const response = {message:"Notes deleted " +`id:${req.body.id}`};
